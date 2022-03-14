d7d742c2c9b37ae595b5878881c8263f78a5a677
stdmatt <stdmatt@pixelwizards.io>
2022-03-14 19:54:53 -0300

    [make-project-v1.0.0] Basic template

---
+++ README.md
@@ -0,0 +1,24 @@
+# ${__PROJECT_NAME__}
+
+**Made with <3 by [stdmatt](http://stdmatt.com).**
+
+<!--  -->
+## Description:
+
+As usual, you are **very welcomed** to **share** and **hack** it.
+
+<!--  -->
+## Dependencies:
+
+* [demolib](https://stdmatt.com/projects/libs/js/demolib.html)
+* [demolib_loader](https://stdmatt.com/projects/libs/js/demolib_loader.html)
+
+<!--  -->
+## License:
+
+This software is released under [GPLv3](https://www.gnu.org/licenses/gpl-3.0.en.html).
+
+<!--  -->
+## Others:
+
+There's more FLOSS things at [stdmatt.com](https://stdmatt.com) :)
---
+++ source/main.js
@@ -0,0 +1,93 @@
+//~---------------------------------------------------------------------------//
+//                        _      _                 _   _                      //
+//                    ___| |_ __| |_ __ ___   __ _| |_| |_                    //
+//                   / __| __/ _` | '_ ` _ \ / _` | __| __|                   //
+//                   \__ \ || (_| | | | | | | (_| | |_| |_                    //
+//                   |___/\__\__,_|_| |_| |_|\__,_|\__|\__|                   //
+//                                                                            //
+//  File      : main.js                                                       //
+//  Project   : //
+//  Date      : //
+//  License   : GPLv3                                                         //
+//  Author    : stdmatt <stdmatt@pixelwizards.io>                             //
+//  Copyright : stdmatt 2022                                                  // 
+//                                                                            //
+//  Description :                                                             //
+//                                                                            //
+//---------------------------------------------------------------------------~//
+
+//------------------------------------------------------------------------------
+__SOURCES = [
+    "/modules/demolib/modules/external/chroma.js",
+    "/modules/demolib/modules/external/perlin.js",
+    "/modules/demolib/source/demolib.js",
+]
+
+const C = {}; // Constants
+const G = {}; // Globals
+
+
+//----------------------------------------------------------------------------//
+// demolib boilerplate                                                        //
+//----------------------------------------------------------------------------//
+//------------------------------------------------------------------------------
+function setup_demo_mode()
+{
+    return new Promise((resolve, reject)=> {
+        demolib_load_all_scripts(__SOURCES).then(()=> {
+            canvas = document.createElement("canvas");
+
+            canvas.width            = window.innerWidth;
+            canvas.height           = window.innerHeight;
+            canvas.style.position   = "fixed";
+            canvas.style.left       = "0px";
+            canvas.style.top        = "0px";
+            canvas.style.zIndex     = "-100";
+
+            document.body.appendChild(canvas);
+            resolve(canvas);
+        });
+    });
+}
+
+//------------------------------------------------------------------------------
+function demo_start(user_canvas)
+{
+    if(!user_canvas) {
+        setup_demo_mode().then((_created_canvas)=>{
+            setup_common(_created_canvas);
+        });
+    } else {
+        setup_common(user_canvas);
+    }
+}
+
+
+//------------------------------------------------------------------------------
+function setup_common(canvas)
+{
+    set_random_seed(null);
+    set_noise_seed (null);
+
+    set_main_canvas       (canvas);
+    install_input_handlers(canvas);
+
+
+    //
+    // Init :)
+    //
+
+    start_draw_loop(update_demo);
+}
+
+//------------------------------------------------------------------------------
+function update_demo(dt)
+{
+
+    begin_draw();
+
+    set_canvas_fill("black");
+    clear_canvas();
+
+    end_draw()
+}
